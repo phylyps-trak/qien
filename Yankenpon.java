06b32249d551cfe4f0aa631e20c4af905e4d8532
M1ch13l <m1ch13l@M1ch13ls-MacBook-Pro.local>
2020-02-16 11:49:52 +0100

    160220 1149

--- Yankenpon.java
+++ Yankenpon.java
@@ -8,26 +8,39 @@
  * om verwarring te voorkomen: throw betekend hier niet "javaklets over if statement op afstand",
  * maar: de shape die de speler "gooit" in het spel. 
  * 
- * VERSIE 2: 15/02: als je allemaal classes maakt werkt het eens stuk beter.
- * VERSIE 3: nu met ROBOT speler
- * VERSIE 4: nu met repeat ja/nee
- * moet nog:
- * -->> VERSIE 5: als je iets anders dan x p r intiept zegt i: doe ff normaal joh
- * -->> VERSIE 6: met moeilijke ASCII
- * -->> VERSIE 7: rock paper scissors lizard spock?
+ * VERSIE 2: 15/02 als je allemaal classes maakt werkt het eens stuk beter.
+ * VERSIE 3: 15/02 nu met random ROBOT speler
+ * VERSIE 4: 15/02 nu met repeat ja/nee
+ * VERSIE 4.2: 16/02 als je bij repeat n, of iets raars intiept kan je het nogmaals doen
+ * VERSIE 5: 16/02 als je iets anders dan x p r intiept zegt i: doe ff normaal joh
+ * TODO -->> VERSIE 6: met moeilijke ASCII
+ * TODO -->> VERSIE 7: rock paper scissors lizard spock?
+ * TODO -->> speler naam dynamisch opbouwen. 
+ * Dus overal waar je nu "Dawa" of "Robbie" print, wil je iets van speler.naam. 
+ * En dan bouw je een speler op met Speler speler1 = new Speler('dawa')
+ * en dan als je 't helemaal fancy wil maken: RobotSpeler als subclass van Speler, 
+ * met z'n eigen methode om een gooi te maken (want random, en niet met input), 
+ * maar dezelfde code voor z'n naam!
+ * Je Class-structure is dan dus misschien: Speler, met twee subclasses: MensSpeler en RobotSpeler
+
+	Speler speler1 = new Speler('dawa'); // Of nog cooler, de user zelf zn naam laten invullen
+	Speler speler2 = new RobotSpeler('robbie');
  */
+//import java.util.HashMap;
 import java.util.Random;
 import java.util.Scanner;  // Import the Scanner class
+
 public class Yankenpon {
 	static Scanner sc = new Scanner(System.in);
 	public static void main(String[] args) {
+		//new Spel().relaties();
 
 		//do-while loop ding zodat je vaker dan 1 keer kan spelen
 		do {
-		// de spelers komen binnen gewaggeld
+			// de spelers komen binnen gewaggeld
 			Speler1 dawa = new Speler1();
-		//Speler2 jaap = new Speler2(); //Jaap heb geen zin meer
-		//maar Robbie de robot wel
+			//Speler2 jaap = new Speler2(); //Jaap heb geen zin meer
+			//maar Robbie de robot wel
 			Speler3 robbie = new Speler3();
 			new Spel().welkom();
 
@@ -35,7 +48,7 @@ public class Yankenpon {
 			//jaap.speler2();
 			robbie.speler3();
 			Spel.uitkomst(dawa.getter1(), robbie.getter3());
-		} while (Spel.stopDoor().equals("j"));
+		} while (new Spel().stopDoor().equals("j"));
 
 
 		sc.close();
@@ -44,6 +57,28 @@ public class Yankenpon {
 }//endofyankenpon
 
 class Spel{
+	/*
+	//HASH MAP met relationele dingen
+	void relaties(){
+		{
+			System.out.println("DEBUG");
+		}
+	// Create a HashMap object
+    HashMap<String, String> moves = new HashMap<String, String>();
+    // Add keys and values
+    moves.put("r", "x");
+    moves.put("p", "r");
+    moves.put("x", "p");
+    //DEBUG
+    for (String i : moves.keySet()) {
+      System.out.println("key: " + i + " value: " + moves.get(i));
+    // TODO 
+      mijnThrow = 'x'
+    		  jouwThrow = 'p'
+    		  moves.get(mijnThrow) == jouwThrow
+    }
+	}
+	 */
 	// introductie & uitleg
 	//char x = '¥';
 	//char p = '▒';
@@ -65,7 +100,7 @@ class Spel{
 		String kP2 = b;
 		// GELIJKSPEL
 		if(kP1.contentEquals(kP2)) {
-			System.out.println("\n** Gelijkspel, vet saai! **");
+			System.out.println("** Gelijkspel, vet saai! **");
 
 			// ALLE ANDERE MOGELIJKHEDEN	
 			// X > P
@@ -88,16 +123,24 @@ class Spel{
 			System.out.println("** You win! **\n  (want "+ kP1 + " beats " +  kP2 + ")");
 		}
 	}
-	//DOORGAAN OF STOPPEN
-	static String stopDoor() {
+	String con;
 
+	//DOORGAAN OF STOPPEN
+	String stopDoor() {
+		{
 		System.out.println("\n================");
 		System.out.println("Ben je in voor een revanche?\n  (type j / n)");
-		// if (sc.hasNextLine()) {
-		String con = Yankenpon.sc.nextLine(); 
-		//  } else {
-		//      System.out.println("\nSorry, je moet iets anders typen");
-		//   }
+		}
+		do {
+		this.con = Yankenpon.sc.nextLine(); 
+		if (con.contentEquals("n")) {
+			System.out.println("Nou, ga maar lekker iets anders doen dan...");
+		} else if (con.contentEquals("j")) {
+		      System.out.println("\n...here we go again!\n");
+	    } else {
+	    	System.out.println("Je invoer slaat nergens op, gek!");
+	    }
+		} while (!con.contentEquals("j"));
 		return con;
 	}	
 }//endofspel
@@ -108,12 +151,9 @@ class Speler1{
 
 	void speler1() {
 		System.out.println("Dawa, voer hier je keuze in:");
-
-		//Scanner scannerPlayerOne = new Scanner(System.in);  // Create a Scanner object
 		this.p1 = Yankenpon.sc.nextLine();  // Read user input
 		System.out.println("Jouw throw is: " + p1);  // Output user input
 		System.out.println("================\n");
-		//scannerPlayerOne.close();
 	}//endofspeler1
 
 	public String getter1() {
@@ -121,33 +161,15 @@ class Speler1{
 	}//endofgetter
 }//endofSpeler1
 
-//SPELER2 ----> zit er niet meer in
-/*
-class Speler2{
-	private String p2;
-
-	void speler2() {
-		System.out.println("Player Two, voer hier je keuze in:");
-
-		Scanner scannerPlayerTwo = new Scanner(System.in);  // Create a Scanner object
-
-		this.p2 = scannerPlayerTwo.nextLine();  // Read user input
-		System.out.println("Je throw is: " + p2);  // Output user input
-		scannerPlayerTwo.close();
-	}//endofspeler2
-
-	public String getter2() {
-		return p2;
-	}//endofgetter
-
-}//endofSpeler2
- */
 //SPELER3: ROBOT
 class Speler3{
+
 	//hier wordt de uitkomst in opgeslagen
 	private String p3;
+
 	//maak een array met x p r
 	String[] opties = {"x","p","r"};
+
 	//hier wordt het random getal in opgeslagen
 	int gekozen = verkrijggetalonder3();
 
@@ -161,19 +183,19 @@ class Speler3{
 
 	void speler3() {
 		System.out.println("Robbie de Robot maakt een pseudo-random keuze:");
-		//DEBUG
-		//for (String keuze : opties) {
-		//	   System.out.println("Dit zijn de mogelijkheden: " + keuze);}
 
 		//trek een random waarde uit "opties" &
 		//gooi die waarde als "keuze speler 3" naar de uitkomst methode: robot.getter3 ofzo
 		this.p3 = opties[gekozen];
+
 		System.out.println("Hij doet --> " + p3 + "\n");
 	}//endofspeler3
+
 	//getter method
 	public String getter3() {
 		return p3;
 	}//endofgetter
+
 }//endofSpeler3
 
 
