e04b72b6b06162205895a5412ece64526b553af9
Michiel <m1ch13l@M1ch13ls-MacBook-Pro.local>
2020-02-19 21:32:41 +0100

    190220 2132

--- Mastermind/Robot.java
+++ Mastermind/Robot.java
@@ -17,42 +17,28 @@ public class Robot {
 		}
 		return geheimeCode;
 
-}//==========
-	void vergelijkDeGooi(String[] gc) {
+}//endof robotCodeMaken
+	//==========
+	void vergelijkDeGooi(String[] gc, Speler s) {
 		//helemaal goed
-		if(geheimeCode[0].equals(gc[0])) {
-			Speler.setGoedCounter(Speler.getGoedCounter() + 1);
-		}else if(geheimeCode[1].equals(gc[1])) {
-			Speler.setGoedCounter(Speler.getGoedCounter() + 1);
-		}else if(geheimeCode[2].equals(gc[2])) {
-			Speler.setGoedCounter(Speler.getGoedCounter() + 1);
-		}else if(geheimeCode[3].equals(gc[3])) {
-			Speler.setGoedCounter(Speler.getGoedCounter() + 1);
-		}else if(geheimeCode[4].equals(gc[4])) {
-			Speler.setGoedCounter(Speler.getGoedCounter() + 1);
-		System.out.println("Je hebt er " + Speler.getGoedCounter() + " goed...");	 
+		for (int i = 0 ; i < gc.length ; i++) {
+		if(geheimeCode[i].equals(gc[i])) {
+			s.setGoedCounter(1);
+			
+		System.out.println("Je hebt er " + s.getGoedCounter() + " goed...");	 
 		}else {
 			System.out.println("...dat lijkt er helemáál niet op!");
 		}
-	
-		Speler.turnCounter--;
-		System.out.println("(Oh nee! Je hebt nog maar " + turnCounter + " beurten om de code te kraken!)");
-		
-		boolean a = Arrays.asList(gokje).contains(geheimeCode[0]);
-		if (a)bijnaCounter++;
-		boolean b = Arrays.asList(gokje).contains(geheimeCode[1]);
-		if (b)bijnaCounter++;
-		boolean c = Arrays.asList(gokje).contains(geheimeCode[2]);
-		if (c)bijnaCounter++;
-		boolean d = Arrays.asList(gokje).contains(geheimeCode[3]);
-		if (d)bijnaCounter++;
-		boolean e = Arrays.asList(gokje).contains(geheimeCode[4]);
-		if (e)bijnaCounter++;
-	
-		System.out.println("DEBUG1 " + a +  b + c + d + e +"\n");
-		System.out.println("Je hebt er " + Speler.getBijnaCounter() + " bijna goed...");
+	}
+
+		for (int i = 0 ; i < gc.length ; i++) {
+		boolean a = Arrays.asList(s.gokje).contains(geheimeCode[i]);
+		if (a)s.bijnaCounter++;
+		s.turnCounter--;
+		System.out.println("(Oh nee! Je hebt nog maar " + s.turnCounter + " beurten om de code te kraken!)");
+		System.out.println("Je hebt er " + s.getBijnaCounter() + " bijna goed...");
 		//boolean contains = Arrays.stream(gokje).anyMatch(geheimeCode[0]::equals);
 		//System.out.println("DEBUG2 " + contains + "\n");
-
+		}
 	}//endof vergelijkDeGooi
 }
